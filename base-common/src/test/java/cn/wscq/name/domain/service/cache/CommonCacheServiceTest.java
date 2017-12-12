ed7a1397a760231a369b98cc6de16afc5eac264f
q353888224 <shuai.zhang@monstar-lib-cn.com>
2017-12-12 16:44:59 +0800

    travis环境

---
+++ base-common/src/test/java/cn/wscq/name/domain/service/cache/CommonCacheServiceTest.java
@@ -0,0 +1,54 @@
+package cn.wscq.name.domain.service.cache;
+
+import cn.wscq.name.BaseTest;
+import cn.wscq.name.MyTestsConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author shuai
+ * @version 1.0
+ * @description
+ * @date 2017/12/12 14:10
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Import(MyTestsConfiguration.class)
+public class CommonCacheServiceTest implements BaseTest{
+
+    @Autowired
+    private CommonCacheService commonCacheService;
+
+    @Test
+    public void saveString() {
+        System.out.println("test");
+    }
+
+    @Test
+    public void saveString1() {
+    }
+
+    @Test
+    public void saveObject() {
+    }
+
+    @Test
+    public void saveObject1() {
+    }
+
+    @Test
+    public void getString() {
+    }
+
+    @Test
+    public void getObject() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
